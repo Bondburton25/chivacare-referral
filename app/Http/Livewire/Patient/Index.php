e5304b0b31a39ced4b4cbeb902988143da02e08e
bondburton25 <kraiwit.dev@gmail.com>
2023-09-18 15:09:20 +0700

    updated patient list

--- app/Http/Livewire/Patient/Index.php
+++ app/Http/Livewire/Patient/Index.php
@@ -28,7 +28,7 @@ class Index extends Component
 
     public function render()
     {
-        $stages   = Stage::all();
+        $stages = Stage::all();
         $health_statuses = HealthStatus::all();
 
         // $date = Carbon::today()->subDays(300);
@@ -36,30 +36,30 @@ class Index extends Component
         // $date_30_days_ago = Carbon::today()->subDays(30);
         // dd($date_30_days_ago);
         // $date_30_days_ago = Carbon::now()->subDays(30);
-        $patients = Patient::where('arrive_date_time', '<=', Carbon::now()->subDays(30))->paginate(10);
+        // $patients = Patient::where('arrive_date_time', '<=', Carbon::now()->subDays(30))->paginate(10);
 
-        // $patients = Patient::when($this->search, function($query) {
-        //     $query->whereHas('referred_by', function($query) {
-        //             $query->where('name', 'like', '%'.$this->search.'%');
-        //         })
-        //         ->orWhere('first_name', 'like', '%'.$this->search.'%')
-        //         ->orWhere('last_name', 'like', '%'.$this->search.'%')
-        //         ->orWhere('contact_person', 'like', '%'.$this->search.'%')
-        //         ->orWhere('number', 'like', '%'.$this->search.'%')
-        //         ->orWhere('phone_number', 'like', '%'.$this->search.'%');
-        //     })
-        //     ->when($this->byStage, function($query) {
-        //         $query->where('stage_id', $this->byStage);
-        //     })
-        //     ->when($this->byHealthStatus, function($query) {
-        //         $query->where('health_status_id', $this->byHealthStatus);
-        //     })
-        //     ->when($this->byArriveDate, function($query) {
-        //         $query->where('arrive_date_time', '>=', $this->byArriveDate);
-        //     })
+        $patients = Patient::when($this->search, function($query) {
+            $query->whereHas('referred_by', function($query) {
+                    $query->where('name', 'like', '%'.$this->search.'%');
+                })
+                ->orWhere('first_name', 'like', '%'.$this->search.'%')
+                ->orWhere('last_name', 'like', '%'.$this->search.'%')
+                ->orWhere('contact_person', 'like', '%'.$this->search.'%')
+                ->orWhere('number', 'like', '%'.$this->search.'%')
+                ->orWhere('phone_number', 'like', '%'.$this->search.'%');
+            })
+            ->when($this->byStage, function($query) {
+                $query->where('stage_id', $this->byStage);
+            })
+            ->when($this->byHealthStatus, function($query) {
+                $query->where('health_status_id', $this->byHealthStatus);
+            })
+            // ->when($this->byArriveDate, function($query) {
+            //     $query->where('arrive_date_time', '>=', $this->byArriveDate);
+            // })
         // ->where('arrive_date_time', '>', now()->subDays(30))
-        // ->idDescending()
-        // ->paginate(10);
+        ->idDescending()
+        ->paginate(10);
         return view('livewire.patient.index', ['patients' => $patients, 'stages' => $stages, 'health_statuses' => $health_statuses]);
     }
 }
